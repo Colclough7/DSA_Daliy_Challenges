6a2ae3ca3bfb2ee60ea2b854a99e7629fe4b036d
Colclough7 <ericcolclough@gmail.com>
2024-07-17 03:13:37 -0400

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -2,7 +2,7 @@
 //Parameter- String
 //Returns- Return the string 'Hello World!'
 //Example- return 'string'
-/*Pseduo code- a function is returning a string 'Hello World!'
+/*Pseduo code- f()=>'Hello World!"
 */
         
       
