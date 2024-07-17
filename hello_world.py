6d8779013a816b93649c4a582ac28f14d2c6ccbd
Colclough7 <ericcolclough@gmail.com>
2024-07-17 03:12:19 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -3,8 +3,7 @@
 #Parameter - String
 #Returns - Return the string 'Hello World!'
 #Example - return 'string'
-#Pseduo code - function helloWorld():  
-                  #return string
+#Pseduo code -  a function is returning a string 'Hello World!'
 
 def hello_world:
     return "Hello World!"
