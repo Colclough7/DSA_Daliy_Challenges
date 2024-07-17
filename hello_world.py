580615d3f7c843d66e1a4f904b18722b98895091
Colclough7 <ericcolclough@gmail.com>
2024-07-17 02:27:54 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,12 @@
+#Return the string 'Hello World!'
 
+#P- String
+#R- Return the string 'Hello World!'
+#E- return 'string'
+'''
+P- function helloWorld():  
+      return string
+'''
+
+def hello_world:
+    return "Hello World!"
