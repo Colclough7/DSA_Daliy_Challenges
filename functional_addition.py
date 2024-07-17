f84afdda5159a7678edac7c49939a6b2157fbe94
Colclough7 <ericcolclough@gmail.com>
2024-07-17 03:08:10 -0400

    Update and rename Functional_Addition.py to functional_addition.py

---
+++ functional_addition.py
@@ -0,0 +1,6 @@
+#Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+#Parameter - integer
+#Returns - returns a function that adds n to any number
+#Example - n + n = integer
+#Pseudo code - function
