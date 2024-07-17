aa857a7422c291a02343066dca5af0abb7d73a96
Colclough7 <ericcolclough@gmail.com>
2024-07-17 03:18:08 -0400

    Update functional_addition.py

--- functional_addition.py
+++ functional_addition.py
@@ -3,4 +3,9 @@
 #Parameter - integer
 #Returns - returns a function that adds n to any number
 #Example - n + n = integer
-#Pseudo code - function
+#Pseudo code - f(n)=>f2(m)=>n+m=>f2
+
+def add(n):
+  def add_helper(m):
+    return n+m
+  return add_helper
