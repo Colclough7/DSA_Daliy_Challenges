cf99f01de365c0d0c7abcf36f955e7fddf6ca6f2
Colclough7 <ericcolclough@gmail.com>
2024-07-17 02:18:44 -0400

    Create helloWorld.js

---
+++ helloWorld.js
@@ -0,0 +1,15 @@
+// Return the string 'Hello World!'
+//P- String
+//R- Return the string 'Hello World!'
+//E- return 'string'
+/*P- function helloWorld():  
+      return string
+*/
+        
+      
+
+
+
+function helloWorld(){
+return 'Hello World!'
+}
