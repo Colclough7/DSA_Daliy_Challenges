3fd21c15daf9e003856e4ac1ef538cb7e0993013
Colclough7 <ericcolclough@gmail.com>
2024-07-18 00:22:12 -0400

    Update get_sum.py

--- get_sum.py
+++ get_sum.py
@@ -1,1 +1,12 @@
+#Write a function called `getSum` that takes in two numbers and returns the sum of those two numbers.
 
+
+#Parameter - sum of two numbers (integres)
+#Return - integer
+#Example - a + b
+#Pseduo code - a function with two argumments that both are integres returming the sum of those two arguments
+
+
+
+def get_sum(a,b):
+  return a+b 
