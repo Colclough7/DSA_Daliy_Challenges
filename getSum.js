95ac3e320283d7e878cda32804c26f34a20b7425
Colclough7 <ericcolclough@gmail.com>
2024-07-18 00:18:20 -0400

    Update getSum.js

--- getSum.js
+++ getSum.js
@@ -1,1 +1,12 @@
+//Write a function called `getSum` that takes in two numbers and returns the sum of those two numbers.
 
+
+//Parameter - sum of two numbers (integres)
+//Return - integer
+//Example - n + m 
+//Pseduo code - a function with two argumments that both are integres returming the sum of those two arguments
+
+
+function getSum(a,b){
+return a + b
+}
