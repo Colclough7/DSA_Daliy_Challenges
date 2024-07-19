5b7bc69e089938716800cb13d90097e267b612fa
Colclough7 <ericcolclough@gmail.com>
2024-07-19 09:48:45 -0400

    Update ordering.sql

--- ordering.sql
+++ ordering.sql
@@ -1,1 +1,17 @@
+--Your task is to sort the information in the provided table 'companies' by number of employees (high to low). Returned table should be in the same format as provided:
 
+--companies table schema
+
+-- id
+-- ceo
+-- motto
+-- employees
+-- Solution should use pure SQL
+
+--Pseduo code: From the companies table, return the number of employees.
+
+/*  SQL  */
+
+SELECT * 
+FROM companies 
+ORDER BY employees DESC;
