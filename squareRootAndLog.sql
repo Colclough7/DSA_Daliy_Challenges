4f33058c2fa545ecddc9a215d3f78c5b6e2525f0
Colclough7 <ericcolclough@gmail.com>
2024-07-19 10:19:19 -0400

    Update squareRootAndLog.sql

--- squareRootAndLog.sql
+++ squareRootAndLog.sql
@@ -7,7 +7,7 @@
 --number2
 --Return a table with two columns (root, log) where the values in root are the square root of those provided in number1 and the values in log are changed to a base 10 logarithm from those in number2.
 
-
+--PSEUDO CODE: SELECT number1 alise root √number1, number2 alise log LOG(10)*log;
 /* SQL */
 
 
