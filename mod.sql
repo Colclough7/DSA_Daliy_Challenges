294bd3342fd5663f18f22eec064968afa1df71d1
Colclough7 <ericcolclough@gmail.com>
2024-07-20 01:53:06 -0400

    Update mod.sql

--- mod.sql
+++ mod.sql
@@ -1,1 +1,11 @@
+--Given the following table 'decimals':
 
+/** decimals table schema **
+
+id
+number1
+number2
+Return a table with one column (mod) which is the output of number1 modulus number2.*/
+
+
+-- Pseduo code -  Select number1 modulus (%) number2 with the alise of mod from the decimals table.
