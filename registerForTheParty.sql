79aeb2afdba609715e452eec1fb392f581054261
Colclough7 <ericcolclough@gmail.com>
2024-07-21 23:49:23 -0400

    Update registerForTheParty.sql

--- registerForTheParty.sql
+++ registerForTheParty.sql
@@ -13,4 +13,6 @@ You can't attend if the attending column returns anything but true*/
 -- Psedocode - Insert into participants table(name,age,attending)
 --values('name',age,boolean);
 
-
+INSERT INTO participants(name,age,attending)
+VALUES('Dewayne Johnson',51,True);
+SELECT * FROM participants;
