1ac950069856512f9ed687688933c239edb04879
Colclough7 <ericcolclough@gmail.com>
2024-07-22 23:32:14 -0400

    Update lowerCase.sql

--- lowerCase.sql
+++ lowerCase.sql
@@ -7,3 +7,12 @@ name
 birthday
 race
 you need to return the same table where all letters are lowercase in the race column.*/
+
+
+
+--PseduoCode - UPDATE demographics SET race column LOWERCASE(race column);
+
+
+/*  SQL  */
+UPDATE demographics SET race = LOWER(race);
+SELECT * FROM demographics;
