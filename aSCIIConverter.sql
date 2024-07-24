c16b21f882109fcfd4116a64453b6f8c22c6b999
Colclough7 <ericcolclough@gmail.com>
2024-07-24 12:52:00 -0400

    Update aSCIIConverter.sql

--- aSCIIConverter.sql
+++ aSCIIConverter.sql
@@ -9,3 +9,11 @@ race
 you need to return the same table where all text fields (name & race) are changed to the ascii code of their first byte.
 
 e.g. Verlie = 86 Warren = 87 Horace = 72 Tracy = 84*/
+
+
+--Pseduocode - SELECT name, FN_ASCII(name) Alise as NumCodeOfName, race, FN_ASCII(race) Alise as NumCodeOfRace FROM demographics
+
+/*  SQL  */
+SELECT name, ASCII(name) AS NumCodeOfName,race, ASCII(race) AS NumCodeOfRace
+FROM  demographics
+
