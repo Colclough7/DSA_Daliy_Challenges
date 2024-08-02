c68b94f51329f2577ee56cea2d3b79857e9de1f0
Colclough7 <ericcolclough@gmail.com>
2024-08-02 18:04:27 -0400

    Update absoluteValueAndLogToBase.sql

--- absoluteValueAndLogToBase.sql
+++ absoluteValueAndLogToBase.sql
@@ -7,3 +7,5 @@ number2
 Return a table with two columns (abs, log) where the values in abs are the absolute values of number1 and the values in log are values from number2 in logarithm to base 64.*/
 
 
+/*  SQL  */
+SELECT ABS(number1) as abs, LOG(64.0, number2) as log FROM decimals
