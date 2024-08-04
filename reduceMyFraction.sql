19a00f3bf17c32dcfd3d7671934ee283701bbe56
Colclough7 <ericcolclough@gmail.com>
2024-08-03 23:55:40 -0400

    Update reduceMyFraction.sql

--- reduceMyFraction.sql
+++ reduceMyFraction.sql
@@ -1,1 +1,4 @@
+--Write a function which reduces fractions to their simplest form! Fractions will be presented as an array/tuple (depending on the language) of strictly positive integers, and the reduced fraction must be returned as an array/tuple:
+
+
 
