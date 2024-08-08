abe8b235925e0b7478da1d180f491c3ae4b0e531
Colclough7 <ericcolclough@gmail.com>
2024-08-07 23:57:45 -0400

    Update go.sql

--- go.sql
+++ go.sql
@@ -3,17 +3,4 @@
 
 You will be provided with an object(staff) containing the staff names as keys, and the department they work in as values.
 
-Each department has a different boredom assessment score, as follows:
 
-accounts = 1
-finance = 2
-canteen = 10
-regulation = 3
-trading = 6
-change = 6
-IS = 8
-retail = 5
-cleaning = 4
-pissing about = 25
-
-Depending on the cumulative score of the team, return the appropriate sentiment:*/
