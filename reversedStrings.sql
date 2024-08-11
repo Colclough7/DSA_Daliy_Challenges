d83e86b7e0361cfee73282e681655ebdd8c20ae9
Colclough7 <ericcolclough@gmail.com>
2024-08-11 18:58:28 -0400

    Update reversedStrings.sql

--- reversedStrings.sql
+++ reversedStrings.sql
@@ -2,3 +2,6 @@
 
 'world'  =>  'dlrow'
 'word'   =>  'drow'*/
+
+
+-- # write your SQL statement here: you are given a table 'solution' with column 'str', return a table with column 'str' and your result in a column named 'res'.
