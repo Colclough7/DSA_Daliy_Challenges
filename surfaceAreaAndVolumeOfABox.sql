2b520a563b9a9ea92fe0668268c71adef0afb1fe
Colclough7 <ericcolclough@gmail.com>
2024-08-14 00:06:41 -0400

    Update surfaceAreaAndVolumeOfABox.sql

--- surfaceAreaAndVolumeOfABox.sql
+++ surfaceAreaAndVolumeOfABox.sql
@@ -8,3 +8,8 @@
 
 
 /*SQL*/
+
+
+SELECT width,height,depth, (2 * (width * height)) + (2 * (width * depth)) + (2 * (height * depth)) AS area, width * height * depth AS volume
+FROM box 
+ORDER BY area ASC, volume ASC, width ASC, height ASC;
