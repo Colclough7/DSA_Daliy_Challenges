a00eeec805777b4c3b0ab70ce48e8085595d3e36
Colclough7 <ericcolclough@gmail.com>
2024-08-16 23:49:52 -0400

    Update removeFirstAndLastCharacter.sql

--- removeFirstAndLastCharacter.sql
+++ removeFirstAndLastCharacter.sql
@@ -4,3 +4,4 @@ You're given one parameter, the original string.
 You don't have to worry about strings with less than two characters.*/
 
 
+-- # write your SQL statement here: you are given a table 'removechar' with column 's', return a table with column 's' and your result in a column named 'res'.
