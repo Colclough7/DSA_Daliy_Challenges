3cf4d4d0576cb98c22f775ddd1f5a83935ba02ae
Colclough7 <ericcolclough@gmail.com>
2024-08-19 19:48:42 -0400

    Update vowelCount.sql

--- vowelCount.sql
+++ vowelCount.sql
@@ -9,3 +9,11 @@ The input string will only consist of lower case letters and/or spaces.*/
 
 
 /*SQL*/
+
+
+
+SELECT 
+    str, 
+    LENGTH(str) - LENGTH(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(str, 'a', ''), 'e', ''), 'i', ''), 'o', ''), 'u', '')) AS res
+FROM getcount;
+
