fad2e7c9031b26964a616de5cc880556b1f11970
Colclough7 <ericcolclough@gmail.com>
2024-08-23 22:50:38 -0400

    Update convertBooleanValuesToStringsYesOrNo.sql

--- convertBooleanValuesToStringsYesOrNo.sql
+++ convertBooleanValuesToStringsYesOrNo.sql
@@ -2,3 +2,6 @@
 
 
 -- # write your SQL statement here: you are given a table 'booltoword' with column 'bool', return a table with column 'bool' and your result in a column named 'res'.
+
+
+/*SQL*/
