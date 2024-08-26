f20dae165890fd515470b047b108babbb105cb8e
Colclough7 <ericcolclough@gmail.com>
2024-08-25 23:49:40 -0400

    Update evenOrOdd.sql

--- evenOrOdd.sql
+++ evenOrOdd.sql
@@ -8,3 +8,9 @@ Numbers table schema
 Output table schema
 * number  INT
 * is_even STRING*/
+
+
+
+
+
+--# write your SQL SELECT statement here: you are given a table 'numbers' with column 'number', return a table with column 'number' and your result in a column named 'is_even'.
