eae5bab0208e0e7572d2c41e83713b9b1e5b62e6
Colclough7 <ericcolclough@gmail.com>
2024-08-27 00:00:31 -0400

    Update makeUpperCase.sql

--- makeUpperCase.sql
+++ makeUpperCase.sql
@@ -1,6 +1,5 @@
 /*Write a function which converts the input string to uppercase.*/
 
---# write your SQL statement here: you are given a table 'dnastrand' with column 'dna', return a table with column 'dna' and your result in a column named 'res'.
-
+--# write your SQL statement here: you are given a table 'makeuppercase' with column 's', return a table with column 's' and your result in a column named 'res'.
 
 /*SQL*/
