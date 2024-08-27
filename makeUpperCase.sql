6cbe04768bf75ffa3152bbe8c9b1b080a7250b5c
Colclough7 <ericcolclough@gmail.com>
2024-08-27 00:01:13 -0400

    Update makeUpperCase.sql

--- makeUpperCase.sql
+++ makeUpperCase.sql
@@ -3,3 +3,11 @@
 --# write your SQL statement here: you are given a table 'makeuppercase' with column 's', return a table with column 's' and your result in a column named 'res'.
 
 /*SQL*/
+
+
+
+SELECT 
+    s,
+    UPPER(s) AS res
+FROM 
+    makeuppercase;
