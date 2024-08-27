c80454c75f3e10d9af14c6cafdfb4885d9aa5971
Colclough7 <ericcolclough@gmail.com>
2024-08-27 00:00:16 -0400

    Update makeUpperCase.sql

--- makeUpperCase.sql
+++ makeUpperCase.sql
@@ -1,18 +1,4 @@
-/*Description:
-Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
-
-If you want to know more: http://en.wikipedia.org/wiki/DNA
-
-In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". 
-Your function receives one side of the DNA (string, except for Haskell); 
-you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).*/
-
-
-/*Example: (input --> output)
-
-"ATTGC" --> "TAACG"
-"GTAT" --> "CATA"*/
-
+/*Write a function which converts the input string to uppercase.*/
 
 --# write your SQL statement here: you are given a table 'dnastrand' with column 'dna', return a table with column 'dna' and your result in a column named 'res'.
 
