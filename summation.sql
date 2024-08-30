1450524719863590bc34c8868f0f84b096e886a8
Colclough7 <ericcolclough@gmail.com>
2024-08-29 23:56:04 -0400

    Update summation.sql

--- summation.sql
+++ summation.sql
@@ -1,8 +1,9 @@
-/*Description:
-Timmy & Sarah think they are in love, but around where they live, 
-they will only know once they pick a flower each. If one of the flowers 
-has an even number of petals and the other has an odd number of petals it means they are in love.
+/*Write a program that finds the summation of every number from 1 to num. 
+The number will always be a positive integer greater than 0. 
+Your function only needs to return the result, what is shown between parentheses in 
+the example below is how you reach that result and it's not part of it, see the sample tests.
 
-Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.*/
+For example (Input -> Output):
 
-
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)*/
