64f630536b877a17eaa5f2b85ae30fd9bd790a30
Colclough7 <ericcolclough@gmail.com>
2024-09-13 23:16:09 -0400

    Update perpendicularLines.sql

--- perpendicularLines.sql
+++ perpendicularLines.sql
@@ -4,3 +4,8 @@ Note: A perpendicular line is one that forms a 90 degree angle
 
 n will always be greater than or equal to 0
 Examples with 3 and 4 lines*/
+
+
+--# write your SQL statement here: 
+-- you are given a table 'perpendicular' with column 'n'
+-- return a table with this column and your result in a column named 'res'.
