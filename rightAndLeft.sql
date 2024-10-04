b5631139affd977bcf9f5150e31082b2e0167b13
Colclough7 <ericcolclough@gmail.com>
2024-10-03 23:50:22 -0400

    Update rightAndLeft.sql

--- rightAndLeft.sql
+++ rightAndLeft.sql
@@ -22,3 +22,13 @@ Case should be maintained.*/
 
 
 /*  SQL  */
+
+
+
+
+
+SELECT 
+    SUBSTRING(project, 1, commits) AS project,
+    SUBSTRING(address, LENGTH(address) - contributors + 1, contributors) AS address
+FROM 
+    repositories;
