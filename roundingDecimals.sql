1bf48479845d4b9c29c534feda8c5f65f4470609
Colclough7 <ericcolclough@gmail.com>
2024-10-06 21:52:41 -0400

    Update roundingDecimals.sql

--- roundingDecimals.sql
+++ roundingDecimals.sql
@@ -9,3 +9,11 @@ Return a table with two columns (number1, number2), the value in number1 should 
 
 
 /*  SQL  */
+
+
+
+SELECT 
+    FLOOR(number1) AS number1,
+    CEILING(number2) AS number2
+FROM 
+    decimals;
