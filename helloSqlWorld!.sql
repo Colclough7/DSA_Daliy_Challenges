54a62846e6cc5c7b87c33e8d45937d1a46f49b60
Colclough7 <ericcolclough@gmail.com>
2024-10-20 19:52:28 -0400

    Update helloSqlWorld!.sql

--- helloSqlWorld!.sql
+++ helloSqlWorld!.sql
@@ -12,3 +12,15 @@ Return a table with a single column named Greeting with the phrase 'hello world!
 
 
 -- write your select statement to return hello world
+
+
+
+CREATE TABLE Greetings (
+    "Greeting" VARCHAR(255)
+);
+
+INSERT INTO Greetings ("Greeting") 
+VALUES ('hello world!');
+
+SELECT "Greeting" FROM Greetings;
+
