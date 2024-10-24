dff04f7d99b9c4a2a6971b170683fe3297573085
Colclough7 <ericcolclough@gmail.com>
2024-10-23 22:10:01 -0400

    Update findingTheFridayNightDVDRental.sql

--- findingTheFridayNightDVDRental.sql
+++ findingTheFridayNightDVDRental.sql
@@ -1,21 +1,34 @@
-/*For this challenge you need to create a UNION statement, there are two tables ussales and eusales the parent company tracks each sale at its respective location in each table, 
-you must all filter the sale price so it only returns rows with a sale greater than 50.00. You have been tasked with combining that data for future analysis. Order by location (US before EU), then by id.
+/*Write a SQL query using the DVD rental database to find the top 50 customers who rented movies on Friday between 6 pm and midnight (i.e. 18:00 to 23:59). For each customer, the query should return their id, full name, the total number of rentals they made on a Friday evening, and a comma-separated list of the dates (including the time) of those rentals, sorted in descending order.
 
-(us/eu)sales table schema
-id
-name
-price
-card_name
-card_number
-transaction_date
-resultant table schema
-location (EU for eusales and US for ussales)
-id
-name
-price (greater than 50.00)
-card_name
-card_number
-transaction_date*/
+Notes:
+for the sample tests, static dump of DVD Rental Sample Database is used, for the final solution - random tests.
+by "top customers" is meant that list should be sorted by the quantity of rentals from higher to lower and if number of rentals is the same - then by last name of a customer
+Good luck!
+
+Schema
+(not all columns - part of the domain required to solve this kata)
+
+customer table:
+Column       | Type     | Modifiers
+------------ +----------+----------
+customer_id  | integer  | not null
+first_name   | varchar  | not null
+last_name    | varchar  | not null
+rental table:
+Column       | Type      | Modifiers
+-------------+-----------+----------
+rental_id    | integer   | not null
+customer_id  | integer   | not null
+rental_date  | timestamp | not null
+return_date  | timestamp | 
+Desired Output
+The desired output should look like this:
+
+customer_id  | customer_name | friday_rentals | rental_dates
+-------------+---------------+----------------+----------------------------------------------
+   32        | Marcia Dean   | 14             | 2005-08-19 21:12:37, 2005-07-08 19:13:50  ...
+   14        | Hosea Jacobi  | 10             | 2005-08-19 21:31:31, 2005-07-29 19:18:57 ... 
+...*/
 
 
 
