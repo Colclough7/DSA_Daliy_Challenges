6427b487d8cba9eeb020b87f50db6050fe7b4239
Colclough7 <ericcolclough@gmail.com>
2024-11-05 22:42:22 -0500

    Update sQLWithSailorMoon.sql

--- sQLWithSailorMoon.sql
+++ sQLWithSailorMoon.sql
@@ -21,3 +21,17 @@ Keep in mind some senshi were not initiated by a cat guardian and one is not in 
 
 
 /*SQL*/
+
+
+
+SELECT 
+    s.senshi_name AS sailor_senshi,
+    s.real_name_jpn AS real_name,
+    c.name AS cat,
+    sc.school AS school
+FROM 
+    sailorsenshi s
+LEFT JOIN 
+    cats c ON s.cat_id = c.id
+LEFT JOIN 
+    schools sc ON s.school_id = sc.id;
