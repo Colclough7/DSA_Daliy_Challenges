5f4ff93ad2d59b98ca9732084ae8d78c419e2a69
Colclough7 <ericcolclough@gmail.com>
2024-11-05 22:42:05 -0500

    Update sQLWithSailorMoon.sql

--- sQLWithSailorMoon.sql
+++ sQLWithSailorMoon.sql
@@ -15,3 +15,9 @@ school
 Return a results table - sailor_senshi, real_name, cat and school - of all characters, containing each character's high school, their civilian name and the cat who introduced them to their magical crime-fighting destiny.
 
 Keep in mind some senshi were not initiated by a cat guardian and one is not in high school. The field can be left blank if this is the case.*/
+
+
+
+
+
+/*SQL*/
