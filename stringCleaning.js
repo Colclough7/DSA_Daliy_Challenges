e67984ac16a1553d9763e6ad63da1b1f069536c3
Colclough7 <ericcolclough@gmail.com>
2024-11-16 21:47:00 -0500

    Update stringCleaning.js

--- stringCleaning.js
+++ stringCleaning.js
@@ -9,3 +9,7 @@ Your harried co-workers are looking to you for a solution to take this garbled t
 Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
 
 */
+
+
+
+/*JS*/
