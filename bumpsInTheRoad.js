5491272fa80215aba617da2769d97f2880d49335
Colclough7 <ericcolclough@gmail.com>
2024-11-17 19:51:22 -0500

    Update bumpsInTheRoad.js

--- bumpsInTheRoad.js
+++ bumpsInTheRoad.js
@@ -3,3 +3,4 @@
 Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead*/
 
 
+/*JS*/
