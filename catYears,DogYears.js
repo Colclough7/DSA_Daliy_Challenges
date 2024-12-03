4ef2d202a0dbca7d18a8bb5f65fe77ca12fca359
Colclough7 <ericcolclough@gmail.com>
2024-12-02 22:05:35 -0500

    Update catYears,DogYears.js

--- catYears,DogYears.js
+++ catYears,DogYears.js
@@ -22,3 +22,23 @@ Dog Years
 
 
 /*JS*/
+
+
+
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+ let catYears = 15;
+  let dogYears = 15;
+
+  if (humanYears >= 2) {
+    catYears += 9;
+    dogYears += 9;
+  }
+
+  if (humanYears > 2) {
+    catYears += (humanYears - 2) * 4;
+    dogYears += (humanYears - 2) * 5;
+  }
+
+  return [humanYears, catYears, dogYears];
+}
