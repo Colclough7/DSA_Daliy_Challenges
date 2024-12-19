7f8ca7c0a192e2515208f61bd19dd09757a5f725
Colclough7 <ericcolclough@gmail.com>
2024-12-18 22:05:10 -0500

    Update grader.js

--- grader.js
+++ grader.js
@@ -19,3 +19,20 @@ grader(0.6) should be "D"*/
 
 
 /*JS*/
+
+
+
+
+function grader(score) {
+   if (score < 0.6 || score > 1) {
+    return "F";
+  } else if (score >= 0.9) {
+    return "A";
+  } else if (score >= 0.8) {
+    return "B";
+  } else if (score >= 0.7) {
+    return "C";
+  } else {
+    return "D";
+  }
+}
