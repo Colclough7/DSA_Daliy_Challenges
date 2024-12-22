b79e398c583ccf675f6faebbb4eca44fb2ccf40b
Colclough7 <ericcolclough@gmail.com>
2024-12-21 23:14:06 -0500

    Update theOldSwitcheroo.js

--- theOldSwitcheroo.js
+++ theOldSwitcheroo.js
@@ -8,3 +8,9 @@ vowel2index('this is my string') == 'th3s 6s my str15ng'
 vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
 vowel2index('') == ''
 Your function should be case insensitive to the vowels.*/
+
+
+
+
+
+/*JS*/
