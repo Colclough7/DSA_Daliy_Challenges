cee646620a52da857ffbbe8507d87cb1176482b7
Colclough7 <ericcolclough@gmail.com>
2025-01-14 21:43:06 -0500

    Update foldingYourWayToTheMoon.js

--- foldingYourWayToTheMoon.js
+++ foldingYourWayToTheMoon.js
@@ -6,3 +6,9 @@ You know that a piece of paper has a thickness of 0.0001m. Given distance in uni
 Note: Of course you can't do half a fold. You should know what this means ;P
 
 Also, if somebody is giving you a negative distance, it's clearly bogus and you should yell at them by returning null (or whatever equivalent in your language). In Shell please return None.*/
+
+
+
+
+
+/*JS*/
