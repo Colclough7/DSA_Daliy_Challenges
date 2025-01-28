51c0b931ac2a5c209a2a216e92bf84678b2e23ef
Colclough7 <ericcolclough@gmail.com>
2025-01-27 23:52:23 -0500

    Update tipCalculator.js

--- tipCalculator.js
+++ tipCalculator.js
@@ -18,3 +18,38 @@ Because you're a nice person, you always round up the tip, regardless of the ser
 
 
 /*JS*/
+
+
+
+function calculateTip(amount, rating) {
+// Normalize the rating to lower case to handle case insensitivity
+  const ratingLower = rating.toLowerCase();
+
+  // Define the tip percentage based on the rating
+  let tipPercentage;
+
+  switch (ratingLower) {
+    case 'terrible':
+      tipPercentage = 0;
+      break;
+    case 'poor':
+      tipPercentage = 0.05;
+      break;
+    case 'good':
+      tipPercentage = 0.1;
+      break;
+    case 'great':
+      tipPercentage = 0.15;
+      break;
+    case 'excellent':
+      tipPercentage = 0.2;
+      break;
+    default:
+      return "Rating not recognised"; // Return message if rating is unrecognized
+  }
+
+  // Calculate the tip and round it up
+  const tip = Math.ceil(amount * tipPercentage);
+
+  return tip;
+}
