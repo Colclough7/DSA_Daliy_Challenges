7e7869a34594eeff5097f7034f89840b9324fe00
Colclough7 <ericcolclough@gmail.com>
2025-01-28 20:56:31 -0500

    Update uniqueInOrder.js

--- uniqueInOrder.js
+++ uniqueInOrder.js
@@ -11,3 +11,17 @@ uniqueInOrder([1,2,2,3,3])       == [1,2,3]*/
 
 
 /*JS*/
+
+
+
+
+var uniqueInOrder=function(iterable){
+  //your code here - remember iterable can be a string or an array
+    let result = [];
+  for (let i = 0; i < iterable.length; i++) {
+    if (iterable[i] !== iterable[i - 1]) {
+      result.push(iterable[i]);
+    }
+  }
+  return result;
+}
