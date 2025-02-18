f0b408bfc470bdd4a94e45168b95af16ddbd4c1f
Colclough7 <ericcolclough@gmail.com>
2025-02-17 23:56:39 -0500

    Update multiplicationTable.js

--- multiplicationTable.js
+++ multiplicationTable.js
@@ -13,3 +13,19 @@ For the given example, the return value should be:
 
 
 /*JS*/
+
+
+
+function multiplicationTable(size) {
+const table = [];
+
+  for (let i = 1; i <= size; i++) {
+    const row = [];
+    for (let j = 1; j <= size; j++) {
+      row.push(i * j);
+    }
+    table.push(row);
+  }
+
+  return table;
+}
