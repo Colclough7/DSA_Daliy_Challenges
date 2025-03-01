4730590729c1836c466b4d0a634844b3a0fb87c4
Colclough7 <ericcolclough@gmail.com>
2025-02-28 22:38:24 -0500

    Update parseFloat.js

--- parseFloat.js
+++ parseFloat.js
@@ -5,3 +5,22 @@
 
 
 /*JS*/
+
+
+function parseF(s) {
+  // Check if the input is null or undefined
+  if (s === null || s === undefined) {
+    return null;
+  }
+
+  // Try to convert the input to a number using the Number constructor
+  const parsed = Number(s);
+
+  // If the result is NaN, return null
+  if (isNaN(parsed)) {
+    return null;
+  }
+
+  // Return the parsed number if it's valid
+  return parsed;
+}
