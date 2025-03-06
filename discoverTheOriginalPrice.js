84775cf4df46c0929fbaf724877295baf29c2685
Colclough7 <ericcolclough@gmail.com>
2025-03-05 22:24:38 -0500

    Update discoverTheOriginalPrice.js

--- discoverTheOriginalPrice.js
+++ discoverTheOriginalPrice.js
@@ -6,3 +6,8 @@ For example:
 Given an item at $75 sale price after applying a 25% discount, the function should return the original price of that item before applying the sale percentage, which is ($100.00) of course, rounded to two decimal places.
 
 DiscoverOriginalPrice(75, 25) => 100.00M where 75 is the sale price (discounted price), 25 is the sale percentage and 100 is the original price*/
+
+
+
+
+/*JS*/
