d4724271f6b1e45f70eb5c93ae61131b40a45469
Colclough7 <ericcolclough@gmail.com>
2025-03-10 23:56:30 -0400

    Update sorted?Yes?No?How?.js

--- sorted?Yes?No?How?.js
+++ sorted?Yes?No?How?.js
@@ -4,3 +4,8 @@
 "yes, descending" - if the numbers in the array are sorted in a descending order
 "no" - otherwise
 You can assume the array will always be valid, and there will always be one correct answer.*/
+
+
+
+
+/*JS*/
