e77df5c8c3750e16e238650f351298353cbff0d7
Colclough7 <ericcolclough@gmail.com>
2025-03-12 21:33:33 -0400

    Update simpleBeadsCount.js

--- simpleBeadsCount.js
+++ simpleBeadsCount.js
@@ -9,3 +9,12 @@ If there are less than 2 blue beads return 0.*/
 
 
 /*JS*/
+
+
+
+function countRedBeads(n) {
+    if (n < 2) {
+        return 0;  // If there are fewer than 2 blue beads, return 0.
+    }
+    return (n - 1) * 2;  // There are (n - 1) gaps between blue beads, and each gap has 2 red beads.
+}
