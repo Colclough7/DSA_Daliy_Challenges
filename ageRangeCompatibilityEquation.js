4e0b61ea5d9b243c7a3bb79f945643b5ded2a40e
Colclough7 <ericcolclough@gmail.com>
2025-03-19 21:48:53 -0400

    Update ageRangeCompatibilityEquation.js

--- ageRangeCompatibilityEquation.js
+++ ageRangeCompatibilityEquation.js
@@ -23,3 +23,20 @@ age = 17   =>   15-20*/
 
 
 /*JS*/
+
+
+
+
+function datingRange(age) {
+  let min, max;
+  
+  if (age <= 14) {
+    min = Math.floor(age - 0.10 * age);
+    max = Math.floor(age + 0.10 * age);
+  } else {
+    min = Math.floor(age / 2 + 7);
+    max = Math.floor((age - 7) * 2);
+  }
+
+  return `${min}-${max}`;
+}
