2098cb990f30d32865da8f1aba72c3735e2ce133
Colclough7 <ericcolclough@gmail.com>
2025-04-08 20:29:45 -0400

    Update regexPasswordValidation.js

--- regexPasswordValidation.js
+++ regexPasswordValidation.js
@@ -12,3 +12,9 @@ only contains alphanumeric characters (note that '_' is not alphanumeric)*/
 
 
 /*JS*/
+
+
+
+
+// assign your RegExp to REGEXP
+const REGEXP = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$/;
