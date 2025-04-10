a891d90b67b26367c5af058775b25908a7c7f1eb
Colclough7 <ericcolclough@gmail.com>
2025-04-10 15:31:31 -0400

    Update allStarCodeChallenge22.js

--- allStarCodeChallenge22.js
+++ allStarCodeChallenge22.js
@@ -20,3 +20,12 @@ For example:
 
 
 /*JS*/
+
+
+
+
+function toTime(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  return `${hours} hour(s) and ${minutes} minute(s)`;
+}
