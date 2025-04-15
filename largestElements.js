09a5560418efc92078b32f53f6db88a1bdc9fc13
Colclough7 <ericcolclough@gmail.com>
2025-04-15 18:43:57 -0400

    Update largestElements.js

--- largestElements.js
+++ largestElements.js
@@ -9,3 +9,14 @@ largest(2, [7,6,5,4,3,2,1])
 
 
 /*JS*/
+
+
+
+
+
+function largest(n, array) {
+  // Sort the array in descending order and take the first n elements
+  let topN = array.sort((a, b) => b - a).slice(0, n);
+  // Return them in ascending order
+  return topN.sort((a, b) => a - b);
+}
