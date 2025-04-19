2f9aece22a4cd90bb65b7f6d0b0f54a7b76240e6
Colclough7 <ericcolclough@gmail.com>
2025-04-18 21:33:37 -0400

    Update objectSyntaxDebug.js

--- objectSyntaxDebug.js
+++ objectSyntaxDebug.js
@@ -2,3 +2,4 @@
 While making a zork-type game, you create an object of rooms. Unfortunately, the game is not working. Find all of the errors in the rooms object to get your game working again.*/
 
 
+/*JS*/
