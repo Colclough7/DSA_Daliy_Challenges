19e98d597bf95c1c14666cf371c87a27c0537454
Colclough7 <ericcolclough@gmail.com>
2025-04-21 23:59:14 -0400

    Update flatten.js

--- flatten.js
+++ flatten.js
@@ -5,3 +5,13 @@ Your function must only do one level of flattening.*/
 /*flatten([1,2,3]) // => [1,2,3]
 flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
 flatten([[[1,2,3]]]) // => [[1,2,3]]*/
+
+
+
+/*JS*/
+
+
+
+function flatten(arr) {
+  return arr.reduce((flat, item) => flat.concat(item), []);
+}
