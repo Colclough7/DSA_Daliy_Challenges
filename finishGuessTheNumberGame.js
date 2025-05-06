96afe53831f05ffffd14483bee0b45974fc608df
Colclough7 <ericcolclough@gmail.com>
2025-05-06 18:17:26 -0400

    Update finishGuessTheNumberGame.js

--- finishGuessTheNumberGame.js
+++ finishGuessTheNumberGame.js
@@ -10,3 +10,25 @@ Can you finish the game so all the rules are met?*/
 
 
 /*JS*/
+
+
+
+class Guesser {
+  constructor(number, lives) {
+    this.number = number;
+    this.lives = lives;
+  }
+  
+  guess(n) {
+    if (this.lives <= 0) {
+      throw new Error("No more lives left. Game over.");
+    }
+
+    if (n === this.number) {
+      return true;
+    } else {
+      this.lives--;
+      return false;
+    }
+  }
+}
