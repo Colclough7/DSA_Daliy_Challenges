4e1b837e736e04cbe68cb890f6826890adfcb713
Colclough7 <ericcolclough@gmail.com>
2025-05-11 23:52:58 -0400

    Update dictionaryFromTwoLists.js

--- dictionaryFromTwoLists.js
+++ dictionaryFromTwoLists.js
@@ -16,3 +16,12 @@ createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3}
 
 
 /*JS*/
+
+
+function createDict(keys, values) {
+  const result = {};
+  for (let i = 0; i < keys.length; i++) {
+    result[keys[i]] = i < values.length ? values[i] : null;
+  }
+  return result;
+}
