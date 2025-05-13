b778c445e47bd1353e15b094aaaac0869596b9a8
Colclough7 <ericcolclough@gmail.com>
2025-05-12 23:20:03 -0400

    Update onlineRPG.js

--- onlineRPG.js
+++ onlineRPG.js
@@ -13,3 +13,13 @@ Otherwise return, False/false (according to the language in use).*/
 
 
 /*JS*/
+
+
+
+function playerRankUp(points) {
+  if (points >= 100) {
+    return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.";
+  } else {
+    return false;
+  }
+}
