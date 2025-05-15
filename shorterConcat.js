f77d2bb8e2ff49346bc4aff0529ca9a9ad14f1bb
Colclough7 <ericcolclough@gmail.com>
2025-05-15 19:27:06 -0400

    Update shorterConcat.js

--- shorterConcat.js
+++ shorterConcat.js
@@ -5,3 +5,10 @@ In other words, the shortest string has to be put as prefix and as suffix of the
 
 Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
 If a and b have the same length treat a as the longer producing b+reverse(a)+b*/
+
+
+
+
+
+
+/*JS*/
