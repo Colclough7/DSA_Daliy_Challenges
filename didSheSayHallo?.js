a99c1fdc6a2f0094ba50a235f5e1a5012dff3586
Colclough7 <ericcolclough@gmail.com>
2025-05-16 22:58:39 -0400

    Update didSheSayHallo?.js

--- didSheSayHallo?.js
+++ didSheSayHallo?.js
@@ -17,3 +17,7 @@ you can assume the input is a string.
 to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
 function should be case insensitive to pass the tests*/
 
+
+
+
+/*JS*/
