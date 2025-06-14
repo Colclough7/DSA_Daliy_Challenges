6966fa410a7c0993d6908c111d66e89d06dbdb7a
Colclough7 <ericcolclough@gmail.com>
2025-06-14 18:50:14 -0400

    Update head,Tail,InitAndLast.js

--- head,Tail,InitAndLast.js
+++ head,Tail,InitAndLast.js
@@ -1,1 +1,14 @@
+/*Your job is to implement these functions in your given language. Make sure it doesn't edit the array; that would cause problems! Here's a cheat sheet:
 
+| HEAD | <----------- TAIL ------------> |
+[  1,  2,  3,  4,  5,  6,  7,  8,  9,  10]
+| <----------- INIT ------------> | LAST |
+
+head [x] = x
+tail [x] = []
+init [x] = []
+last [x] = x
+Here's how I expect the functions to be called in your language:
+
+head([1,2,3,4,5]); => 1
+tail([1,2,3,4,5]); => [2,3,4,5]*/
