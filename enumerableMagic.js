e7e0db97bbf09773b6a97c60c40ca15a215e5f8c
Colclough7 <ericcolclough@gmail.com>
2025-06-24 21:38:25 -0400

    Update enumerableMagic.js

--- enumerableMagic.js
+++ enumerableMagic.js
@@ -1,1 +1,5 @@
+/*Write a function that takes two arguments: an array and a callback function (in Ruby: a block).
+
+The function should return true if the callback / block returns false for all of the items in the array, or if the array is empty; otherwise return false.*/
+
 
