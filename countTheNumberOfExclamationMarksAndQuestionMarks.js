8c6f77346e8384e95e303c03324df4bf2d1b8542
Colclough7 <ericcolclough@gmail.com>
2025-07-03 21:02:06 -0400

    Update countTheNumberOfExclamationMarksAndQuestionMarks.js

--- countTheNumberOfExclamationMarksAndQuestionMarks.js
+++ countTheNumberOfExclamationMarksAndQuestionMarks.js
@@ -1,1 +1,14 @@
+/*Description:
+Count the number of exclamation marks and question marks, return the product.
 
+Examples
+""          --->   0
+"!"         --->   0
+"!ab? ?"    --->   2
+"!!"        --->   0
+"!??"       --->   2
+"!???"      --->   3
+"!!!??"     --->   6
+"!!!???"    --->   9
+"!???!!"    --->   9
+"!????!!!?" --->  20*/
