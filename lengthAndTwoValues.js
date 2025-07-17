db57c1545b2d2dcb3033d903552243903e23dd57
Colclough7 <ericcolclough@gmail.com>
2025-07-17 16:19:04 -0400

    Update lengthAndTwoValues.js

--- lengthAndTwoValues.js
+++ lengthAndTwoValues.js
@@ -11,3 +11,13 @@ Good luck!*/
 
 
 /*JS*/
+
+
+
+function alternate(n, firstValue, secondValue) {
+  let result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(i % 2 === 0 ? firstValue : secondValue);
+  }
+  return result;
+}
