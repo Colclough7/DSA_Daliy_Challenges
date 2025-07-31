781e969bc1a0ec784920f5fe2ce9c6ee598d3c2d
Colclough7 <ericcolclough@gmail.com>
2025-07-31 17:38:15 -0400

    Update methodsOfNumberObject.js

--- methodsOfNumberObject.js
+++ methodsOfNumberObject.js
@@ -70,4 +70,4 @@ for example:
 
 howManySmaller([1.234,1.235,1.228],1.24) should return 2
 howManySmaller([1.1888,1.1868,1.1838],1.19) should return 1
-howManySmaller([3.1288,3.1212,3.1205],3.1212) should return 2
+howManySmaller([3.1288,3.1212,3.1205],3.1212) should return 2*/
