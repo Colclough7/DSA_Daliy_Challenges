29b57f9610ce94f31f33d82618df48a7652d10f6
Colclough7 <ericcolclough@gmail.com>
2025-08-07 22:26:22 -0400

    Update sumAListButIgnoreAnyDuplicates.js

--- sumAListButIgnoreAnyDuplicates.js
+++ sumAListButIgnoreAnyDuplicates.js
@@ -8,3 +8,25 @@ and for the list [1, 10, 3, 10, 10] the function should return 4.*/
 
 
 /*JS*/
+
+
+
+function sumNoDuplicates(numList) {
+  // Create an object to count the frequency of each number
+  const counts = {};
+
+  // Count each number
+  for (let num of numList) {
+    counts[num] = (counts[num] || 0) + 1;
+  }
+
+  // Sum only numbers that appear exactly once
+  let sum = 0;
+  for (let num of numList) {
+    if (counts[num] === 1) {
+      sum += num;
+    }
+  }
+
+  return sum;
+}
