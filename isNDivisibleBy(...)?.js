44960f5374937a325b9f51ffc1511b29626b13cf
Colclough7 <ericcolclough@gmail.com>
2025-08-09 18:20:17 -0400

    Update isNDivisibleBy(...)?.js

--- isNDivisibleBy(...)?.js
+++ isNDivisibleBy(...)?.js
@@ -11,3 +11,15 @@ Example:
 
 
 /*JS*/
+
+
+
+
+function isDivisible(n, ...args) {
+  for (let i = 0; i < args.length; i++) {
+    if (n % args[i] !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
