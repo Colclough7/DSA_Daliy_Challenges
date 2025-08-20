04916fa3ffb6a0246e96cb216d1da345e67a283f
Colclough7 <ericcolclough@gmail.com>
2025-08-19 20:11:54 -0400

    Update unscrambledEggs.js

--- unscrambledEggs.js
+++ unscrambledEggs.js
@@ -13,3 +13,13 @@ Kata is supposed to be for beginners to practice reggular eggspressions, so comm
 
 
 /*JS*/
+
+
+
+
+function unscrambleEggs(word) {
+  // This regex replaces every occurrence of "egg" following a consonant
+  // ([^aeiou]) is any character that's not a vowel (case-sensitive)
+  // We use a capturing group to keep the consonant
+  return word.replace(/([^aeiouAEIOU])egg/g, '$1');
+}
