73336d9a4e418221486b53ac0fd197c16f470907
Colclough7 <ericcolclough@gmail.com>
2025-08-24 18:26:26 -0400

    Update whichIsGreater?.js

--- whichIsGreater?.js
+++ whichIsGreater?.js
@@ -7,3 +7,7 @@ If the sum of odd numbers is greater than the sum of even numbers return: "Odd i
 If the total of both even and odd numbers are identical return: "Even and Odd are the same"*/
 
 
+
+
+
+/*JS*/
