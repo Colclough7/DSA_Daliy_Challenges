85daf45c4fb92406b2115f06df795e1cfb962d4f
Colclough7 <ericcolclough@gmail.com>
2025-08-23 21:10:49 -0400

    Update especiallyJoyfulNumbers.js

--- especiallyJoyfulNumbers.js
+++ especiallyJoyfulNumbers.js
@@ -8,3 +8,8 @@ and 19 * 91 = 1729 --> the number that we started with.
 Complete the function which tests if a positive integer n is Harshad number, and returns True if the product of its digit sum and its digit sum reversed equals n; otherwise return False.*/
 
 
+
+
+
+
+/*JS*/
