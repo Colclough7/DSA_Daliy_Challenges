d6a0912064994beaabdd9b0b0ba66f959e25ed83
Colclough7 <ericcolclough@gmail.com>
2025-09-02 18:42:19 -0400

    Update simplifyingMultilinearPolynomials.js

--- simplifyingMultilinearPolynomials.js
+++ simplifyingMultilinearPolynomials.js
@@ -29,3 +29,48 @@ Good Work :)*/
 
 
 /*JS*/
+
+
+
+function simplify(poly) {
+  const regex = /[+-]?\d*[a-z]+/g;
+  const matches = poly.match(regex) || [];
+  const map = {};
+
+  for (let term of matches) {
+    let sign = 1;
+    if (term[0] === '-') {
+      sign = -1;
+      term = term.slice(1);
+    } else if (term[0] === '+') {
+      term = term.slice(1);
+    }
+
+    let coeff = term.match(/^\d+/);
+    coeff = coeff ? parseInt(coeff[0]) : 1;
+    const vars = term.replace(/^\d+/, '').split('').sort().join('');
+    const key = vars;
+
+    if (!map[key]) map[key] = 0;
+    map[key] += sign * coeff;
+  }
+
+  const sorted = Object.entries(map)
+    .filter(([_, coeff]) => coeff !== 0)
+    .sort((a, b) => {
+      if (a[0].length !== b[0].length)
+        return a[0].length - b[0].length;
+      return a[0] < b[0] ? -1 : 1;
+    });
+
+  let result = '';
+  for (const [vars, coeff] of sorted) {
+    const sign = coeff > 0 ? '+' : '';
+    const absCoeff = Math.abs(coeff);
+    result +=
+      (result === '' && coeff > 0 ? '' : sign) +
+      (absCoeff === 1 ? (vars === '' ? '1' : (coeff < 0 ? '-' : '') + vars) : coeff + vars);
+  }
+
+  return result;
+}
