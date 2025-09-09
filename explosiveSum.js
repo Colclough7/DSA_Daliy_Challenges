b6373233314ebe4b7301a4195acdd068bb05e79b
Colclough7 <ericcolclough@gmail.com>
2025-09-09 18:36:11 -0400

    Update explosiveSum.js

--- explosiveSum.js
+++ explosiveSum.js
@@ -26,3 +26,19 @@ sum(100) // 190569292*/
 
 
 /*JS*/
+
+
+
+
+function sum(n) {
+  const dp = Array(n + 1).fill(0);
+  dp[0] = 1;
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = i; j <= n; j++) {
+      dp[j] += dp[j - i];
+    }
+  }
+
+  return dp[n];
+}
