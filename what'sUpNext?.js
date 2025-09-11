007cd477aec52c82da61b0238b4167ec9cb22255
Colclough7 <ericcolclough@gmail.com>
2025-09-10 22:44:06 -0400

    Update what'sUpNext?.js

--- what'sUpNext?.js
+++ what'sUpNext?.js
@@ -8,3 +8,15 @@ nextItem("testing", "t") # "e"*/
 
 
 /*JS*/
+
+
+
+
+function nextItem(xs, item) {
+  let found = false;
+  for (const x of xs) {
+    if (found) return x;
+    if (x === item) found = true;
+  }
+  return undefined;
+}
