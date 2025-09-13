48cfbcd8ab39aaaec6f4d2913929aede93a11922
Colclough7 <ericcolclough@gmail.com>
2025-09-12 20:57:26 -0400

    Update selectiveFearOfNumbers.js

--- selectiveFearOfNumbers.js
+++ selectiveFearOfNumbers.js
@@ -20,3 +20,26 @@ Write a function which takes a string (day of the week) and an integer (number t
 
 
 /*JS*/
+
+
+
+var AmIAfraid = function(day, num){
+  switch(day) {
+    case "Monday":
+      return num === 12;
+    case "Tuesday":
+      return num > 95;
+    case "Wednesday":
+      return num === 34;
+    case "Thursday":
+      return num === 0;
+    case "Friday":
+      return num % 2 === 0;
+    case "Saturday":
+      return num === 56;
+    case "Sunday":
+      return num === 666 || num === -666;
+    default:
+      return false; // Unknown day
+  }
+};
