1313508297bc4d5de85e74421334126b3641862c
Colclough7 <ericcolclough@gmail.com>
2025-09-17 18:02:16 -0400

    Update balancedNumber.js

--- balancedNumber.js
+++ balancedNumber.js
@@ -32,3 +32,7 @@ middle digit(s): 02
 sum of all digits to the left of the middle digit(s) -> 10
 sum of all digits to the right of the middle digit(s) -> 20
 10 and 20 are not equal, so it's not balanced.*/
+
+
+
+/*JS*/
