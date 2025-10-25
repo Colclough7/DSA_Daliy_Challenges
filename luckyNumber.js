7f64895e89316e1a1479b6d31fd537fc7dfad75e
Colclough7 <ericcolclough@gmail.com>
2025-10-24 20:38:44 -0400

    Fix formatting issues in luckyNumber.js

--- luckyNumber.js
+++ luckyNumber.js
@@ -5,3 +5,9 @@ Write a function to find if a number is lucky or not. If the sum of all digits i
 1892376 => 1+8+9+2+3+7+6 = 36. 36 is divisible by 9, hence number is lucky.
 
 Function will return true for lucky numbers and false for others.*/
+
+
+
+
+
+/*JS*/
