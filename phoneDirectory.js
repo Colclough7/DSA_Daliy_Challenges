3f17c4df84cb16fd47c326d7a618042196a93a38
Colclough7 <ericcolclough@gmail.com>
2025-10-29 21:21:25 -0400

    Add requirements for phone directory functionality
    Add a comment with requirements for phone directory program.

--- phoneDirectory.js
+++ phoneDirectory.js
@@ -1,1 +1,9 @@
+/*Could you help John with a program that, given the lines of his phone book and a phone number num returns a string for this number : "Phone => num, Name => name, Address => adress"
 
+Examples:
+s = "/+1-541-754-3010 156 Alphand_St. <J Steeve>\n 133, Green, Rd. <E Kustur> NY-56423 ;+1-541-914-3010!\n"
+
+phone(s, "1-541-754-3010") should return "Phone => 1-541-754-3010, Name => J Steeve, Address => 156 Alphand St."
+It can happen that there are many people for a phone number num, then return : "Error => Too many people: num"
+
+or it can happen that the number num is not in the phone book, in that case return: "Error => Not found: num"*/
