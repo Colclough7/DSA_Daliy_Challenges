8da048f085e38b2902e42f0c13b6f0329735c052
Colclough7 <ericcolclough@gmail.com>
2025-12-02 22:31:14 -0500

    Update isIntegerArray?.js

--- isIntegerArray?.js
+++ isIntegerArray?.js
@@ -4,3 +4,7 @@ returns true  / True if every element in an array is an integer or a float with 
 returns true  / True if array is empty.
 returns false / False for every other input.*/
 
+
+
+
+/*JS*/
